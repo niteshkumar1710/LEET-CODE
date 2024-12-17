493c5a2a8b31972f17cd427de181ba545685632f
Nitesh kumar <130887835+niteshkumar1710@users.noreply.github.com>
2024-12-18 05:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (31.01%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,10 +4,26 @@ public:
         int max=INT_MIN;
         int currentsum=0;
 
+        if(nums.size()==1){
+            return nums[0];
+        }
+        
         for(int i=0;i<nums.size();i++)
         {
-            
-            currentsum+=nums[i];
+            // if (currentsum+nums[i]<0)
+            // {
+            //     currentsum=0;
+            // }
+            // else{
+            //     currentsum+=nums[i];
+            // }
+
+            // if(currentsum>max){
+            //         max=currentsum;
+            // }
+
+            //the above code is not passing the test case while submiting ,but below code gets submited 
+             currentsum+=nums[i];
 
             if(currentsum>max){
                 max=currentsum;
@@ -15,7 +31,6 @@ public:
             if(currentsum<0){
                 currentsum=0;
             }
-
         }
         return max;
 
