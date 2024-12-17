9e9c99a1a4714bc141d5e25c25ec1a54ff06caaa
Nitesh kumar <130887835+niteshkumar1710@users.noreply.github.com>
2024-12-18 05:18:37 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (21.53%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,9 +4,9 @@ public:
         int max=INT_MIN;
         int currentsum=0;
 
-        if(nums.size()==1){
-            return nums[0];
-        }
+        // if(nums.size()==1){
+        //     return nums[0];
+        // }
         
         for(int i=0;i<nums.size();i++)
         {
