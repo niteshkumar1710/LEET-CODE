f4e56d662b8416e889f038b2a7036c02a459548c
Nitesh kumar <130887835+niteshkumar1710@users.noreply.github.com>
2024-12-19 06:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (30.69%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //     vector<int>hash;
+        //     for(int i=0;i<nums.size();i++)
+        //     {
+        //         hash[nums[i]]+=1;
+        //     }
+        //     int maxElement=0;
+        //     int index;
+        //     for(int i=0;i<hash.size();i++)
+        //     {
+        //         if(hash[i]>maxElement){
+        //             maxElement=hash[i];
+        //             index=i;
+        //         }
+
+        //     }
+        //     return index;
+
+        // }
         unordered_map<int, int> hash; // Hash map to store frequencies
         for (int num : nums) {
             hash[num]++; // Increment the count for this number
         }
-        
+
         int maxElement = 0;
         int index = nums[0]; // Default to the first element
         for (auto& [key, value] : hash) {
