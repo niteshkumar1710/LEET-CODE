09e1bac00f2aacd394b53330a9a4fa566f9f798d
Nitesh kumar <130887835+niteshkumar1710@users.noreply.github.com>
2025-11-22 23:14:07 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.02%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // int j=-1;
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     if(nums[i]==0){
-        //         j=i;
-        //         break;// here break is used to stop when we found the first zero because after this the j always be 0 when we swap because only zero and non zero is swapped
-        //     }
-        // }
-        // //assignign i to the next position of the j to check non zero and swap
-        // for (int i=j+1;i<nums.size();i++)
-        // {
-        //     if(nums[i]!=0){
-        //         swap(nums[j],nums[i]);
-        //         j++;
-        //     }
-        // }
-        int j = 0;  // position to place next non-zero
+        int j=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0){
+                j=i;
+                break;// here break is used to stop when we found the first zero because after this the j always be 0 when we swap because only zero and non zero is swapped
+            }
+        }
+        // if no zero, nothing to do
+        if (j == -1) return;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
+        //assignign i to the next position of the j to check non zero and swap
+        for (int i=j+1;i<nums.size();i++)
+        {
+            if(nums[i]!=0){
+                swap(nums[j],nums[i]);
                 j++;
             }
         }
+        // int j = 0;  // position to place next non-zero
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] != 0) {
+        //         swap(nums[i], nums[j]);
+        //         j++;
+        //     }
+        // }
     }
 };
