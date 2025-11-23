a6f05517d0a761a255401a33cf9ad0c8edb63357
Nitesh kumar <130887835+niteshkumar1710@users.noreply.github.com>
2025-11-23 23:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,19 +1,56 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int midx = m - 1;
-        int nidx = n - 1;
-        int right = m + n - 1;
+        // int midx = m - 1;
+        // int nidx = n - 1;
+        // int right = m + n - 1;
 
-        while (nidx >= 0) {
-            if (midx >= 0 && nums1[midx] > nums2[nidx]) {
-                nums1[right] = nums1[midx];
-                midx--;
-            } else {
-                nums1[right] = nums2[nidx];
-                nidx--;
+        // while (nidx >= 0) {
+        //     if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+        //         nums1[right] = nums1[midx];
+        //         midx--;
+        //     } else {
+        //         nums1[right] = nums2[nidx];
+        //         nidx--;
+        //     }
+        //     right--;
+        // }
+        int i=0;
+        int j=0;
+        vector<int>unionarr;
+        while(i<m&&j<n)
+        {
+          if (nums1[i]<=nums2[j]){
+            if(1)
+            {
+                unionarr.push_back(nums1[i]);
             }
-            right--;
-        }        
+            i++;
+          }
+          else
+          {
+            if(1)
+            {
+                unionarr.push_back(nums2[j]);
+            }
+            j++;
+          }
+        }  
+        while(j<n){
+            if(1)
+            {
+                unionarr.push_back(nums2[j]);
+            }
+            j++;
+        }   
+        while(i<m){
+            if(1)
+            {
+                unionarr.push_back(nums1[i]);
+            }
+            i++;
+        }   
+        nums1=unionarr;
     }
+
 };
