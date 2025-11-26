7b1ebdfd360a1d8cd749fbdfc321881686a55700
Nitesh kumar <130887835+niteshkumar1710@users.noreply.github.com>
2025-11-26 13:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.39%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-       unordered_map<long long,int> m;
-       int val=0;
-       for(int i=0 ; i<nums.size();i++)
-       {
-        m[nums[i]]++;
-       }
-       for(auto it:m)
-       {
-        if(it.second==1)
-        {
-            val= it.first;
-        }
-       }
-       return val;
+    //    unordered_map<long long,int> m;
+    //    int val=0;
+    //    for(int i=0 ; i<nums.size();i++)
+    //    {
+    //     m[nums[i]]++;
+    //    }
+    //    for(auto it:m)
+    //    {
+    //     if(it.second==1)
+    //     {
+    //         val= it.first;
+    //     }
+    //    }
+    //    return val;
        
-        // xor=0
-        // for (int i=0 ; i<nums.size();i++)
-        // {
-        //     xor=xor^nums[i];
-        // }
-        // return xor;
+        int xor1=0;
+        for (int i=0 ; i<nums.size();i++)
+        {
+            xor1=xor1^nums[i];
+        }
+        return xor1;
         
         
     }
