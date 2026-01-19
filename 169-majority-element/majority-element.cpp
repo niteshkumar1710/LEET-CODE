4489139cc6efd69f1c3b8f3643bf151fa909a4ba
Nitesh kumar <130887835+niteshkumar1710@users.noreply.github.com>
2026-01-19 14:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.27%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -17,21 +17,38 @@ public:
 
         //     }
         //     return index;
- 
-        // }
-        unordered_map<int, int> hash; // Hash map to store frequencies
-        for (int num : nums) {
-            hash[num]++; // Increment the count for this number
-        }
 
-        int maxElement = 0;
-        int index = nums[0]; // Default to the first element
-        for (auto& [key, value] : hash) {
-            if (value > maxElement) {
-                maxElement = value;
-                index = key;
+        // }
+        // unordered_map<int, int> hash; // Hash map to store frequencies
+        // for (int num : nums) {
+        //     hash[num]++; // Increment the count for this number
+        // }
+
+        // int maxElement = 0;
+        // int index = nums[0]; // Default to the first element
+        // for (auto& [key, value] : hash) {
+        //     if (value > maxElement) {
+        //         maxElement = value;
+        //         index = key;
+        //     }
+        // }
+        // return index;
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
-        return index;
+
+        return candidate;
     }
 };
